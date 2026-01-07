cd32cfb37e15b79f20d445c782da981de25d1cb7
tulsip1208-del <tulsip1208@gmail.com>
2026-01-07 14:40:44 +0530

    Update ProctorTest.jsx
    auto

--- frontend/src/proctoring/test/ProctorTest.jsx
+++ frontend/src/proctoring/test/ProctorTest.jsx
@@ -5,8 +5,8 @@ export default function ProctorTest() {
     <div style={{ padding: 20 }}>
       <h2>AI Proctoring – Test Page</h2>
       <p>Open console and test face detection</p>
-      <ProctoringEngine  forceSubmitExam={() => alert("AUTO SUBMIT")}
+      <ProctoringEngine  forceSubmitExam={() => alert("AUTO done SUBMIT")}
         showWarningPopup={(msg) => alert(msg)}/>
     </div>
   );
-}
+}      
